37d83267b17d64b42e81b9432879b6cb0e79d5ac
Thurein Zaw <109714248+mhkhizil@users.noreply.github.com>
2023-10-09 21:39:49 +0630

    final update

--- src/Feature/API/dbApi.jsx
+++ src/Feature/API/dbApi.jsx
@@ -15,6 +15,28 @@ export const dbApi = createApi({
         providesTags:['dbApi']
   
       }),
+      getDashboardDataMonthly: builder.query({
+        query: ({token}) => ({
+          url:`/dashboard-overview/monthly`,
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+          
+        }),
+        providesTags:['dbApi']
+  
+      }),
+      getDashboardDataWeekly: builder.query({
+        query: ({token}) => ({
+          url:`/dashboard-overview/weekly`,
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+          
+        }),
+        providesTags:['dbApi']
+  
+      }),
   })
 })
-export const {useGetDashboardDataQuery} = dbApi;
+export const {useGetDashboardDataQuery,useGetDashboardDataMonthlyQuery,useGetDashboardDataWeeklyQuery} = dbApi;
